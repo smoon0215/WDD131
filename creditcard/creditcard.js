395da44a2dd94f80eccce16b13f3b1b020c8ec0e
smoon0215 <moo19034@byui.edu>
2025-02-27 13:40:43 -0700

    W7 all activities

---
+++ creditcard/creditcard.js
@@ -0,0 +1,16 @@
+// Form validation on submit
+// document.getElementById("card-button").addEventListener("click", function(e) {
+//     e.preventDefault(); // Prevent form submission
+
+//     const cardNumber = document.getElementById("card-number").value;
+//     const cardHolder = document.getElementById("card-holder").value;
+//     const expMonth = document.getElementById("card-month").value;
+//     const expYear = document.getElementById("card-year").value;
+//     const cvc = document.getElementById("card-cvc").value;
+
+//     if (!cardNumber || !cardHolder || !expMonth || !expYear || !cvc) {
+//         alert("Please fill out all fields.");
+//     } else {
+//         alert("Form submitted successfully!");
+//     }
+// });

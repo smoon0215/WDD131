51efe86b1c51b408b8d9d4c4d764730c4dc421a9
smoon0215 <moo19034@byui.edu>
2025-04-05 21:17:04 -0600

    Testing because stuff ISNT WORKING

---
+++ moon-gallery/gallery.mjs
@@ -0,0 +1,132 @@
+const pictures = [
+    {//insert info fopr each one
+        Title: 'Partnership', 
+        Description: 'Dreamlike composition blending organic forms with the fragility of a single line portrait.', 
+        Year: '', 
+        image: 'moon-gallery\images\abstract-faces.jpg', 
+        alt: 'A whisper of a face emerges against a backdrop of soft, organic color fields.',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\bear-park.jpg', 
+        alt: '',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: 'Looking Heavenward', 
+        Description: 'A low-angle shot, framing the sky with the dark, organic shapes of the forest canopy.', 
+        Year: '', 
+        image: 'moon-gallery\images\camp.jpg', 
+        alt: 'A low-angle shot, framing the sky with the dark shapes of the forest canopy.',
+        Medium: 'Photograph', 
+
+    },
+    {//insert info fopr each one
+        Title: 'French Idiology', 
+        Description: 'Floral adornment on a vintage lamppost, painted with a soft, atmospheric touch.', 
+        Year: '', 
+        image: 'moon-gallery\images\lanterns.jpg', 
+        alt: 'A solitary lamppost with vibrant flowers, stands against a warm, muted backdrop.',
+        Medium: 'Medium - Oil on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: 'Oakland Temple - In Progress', 
+        Description: 'Underpainting for a religious temple, showing the initial stages of form and color development.', 
+        Year: '', 
+        image: 'moon-gallery\images\large-temple.jpg', 
+        alt: 'Underpainting for a religious temple',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\libro.jpg', 
+        alt: '',
+        Medium: '', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\min-temple.jpg', 
+        alt: '',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\oregon-coast-rocks.jpg', 
+        alt: '',
+        Medium: '', 
+
+    },
+    {//insert info fopr each one
+        Title: 'Perception', 
+        Description: 'A moment captured at the edge of the sea, where the raw beauty of the natural world is on full display.', 
+        Year: '', 
+        image: 'moon-gallery\images\oregon-coast.JPG', 
+        alt: 'A coastal panorama, capturing the rugged textures of tide pools against the vast expanse of the ocean and sky.',
+        Medium: 'Photograph', 
+
+    },
+    {//insert info fopr each one
+        Title: 'Morning Glory', 
+        Description: 'A glimpse into the heart of a redwood ecosystem, showcasing the symbiotic relationship between towering trees and flourishing undergrowth.', 
+        Year: '', 
+        image: 'moon-gallery\images\oregon-trees.jpg', 
+        alt: 'Sunlight bursts through the redwood canopy, illuminating the vibrant ferns and the textured bark.',
+        Medium: 'Photograph', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\ranbow-tree.jpg', 
+        alt: '',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\rolling-hills.jpg', 
+        alt: '',
+        Medium: 'Medium - Oil on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\tempe.jpg', 
+        alt: '',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+    {//insert info fopr each one
+        Title: '', 
+        Description: '', 
+        Year: '', 
+        image: 'moon-gallery\images\red-umbrella.jpg', 
+        alt: 'Red umbrella on a mirrored lake of a dream like sky',
+        Medium: 'Medium - Acrylic on canvas', 
+
+    },
+]
+
+
+export default pictures 
+
